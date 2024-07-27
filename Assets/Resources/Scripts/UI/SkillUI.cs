a92ceee84b6434e2668fbb1f1413f357a781972f
prooni <lhw629@naver.com>
2024-07-27 16:44:13 +0900

    아이템 아이콘 표시
    아이템 아이콘 표시

--- Assets/Resources/Scripts/UI/SkillUI.cs
+++ Assets/Resources/Scripts/UI/SkillUI.cs
@@ -5,6 +5,8 @@ public class SkillUI : UIBase
 {
     private Button button = null;
     private ItemType skillType;
+    [SerializeField] private Image bombIcon = null;
+    [SerializeField] private Image cannonIcon = null;
 
     public delegate void DoSkill(ItemType type);
     public DoSkill doSkill = null;
@@ -14,6 +16,8 @@ public class SkillUI : UIBase
         button = GetComponentInChildren<Button>();
         SetInteractable(false);
         button.onClick.AddListener(OnButtonClick);
+        bombIcon.gameObject.SetActive(false);
+        cannonIcon.gameObject.SetActive(false);
     }
 
     public void SetInteractable(bool isEnable)
@@ -26,6 +30,19 @@ public class SkillUI : UIBase
         skillType = type;
 
         //스킬 아이콘 업데이트 코드 추가 
+        switch(type)
+        {
+            case ItemType.Bomb:
+                bombIcon.gameObject.SetActive(true);
+                cannonIcon.gameObject.SetActive(false);
+                break;
+            case ItemType.Cannon:
+                bombIcon.gameObject.SetActive(false);
+                cannonIcon.gameObject.SetActive(true);
+                break;
+            default:
+                break;
+        }
     }
 
     public void OnButtonClick()
@@ -34,7 +51,8 @@ public class SkillUI : UIBase
         {
             doSkill.Invoke(skillType);
         }
-
+        bombIcon.gameObject.SetActive(false);
+        cannonIcon.gameObject.SetActive(false);
         SetInteractable(false);
     }
 }
